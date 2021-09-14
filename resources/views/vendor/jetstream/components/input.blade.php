fbdf43a1e253d4f723e74532d93e8e64985c529d
Amanda Romero <mandicap@comcast.net>
2021-09-14 08:13:22 -0600

    Warms up the login page
    Signed-off-by: Amanda Romero <mandicap@comcast.net>

--- resources/views/vendor/jetstream/components/input.blade.php
+++ resources/views/vendor/jetstream/components/input.blade.php
@@ -1,3 +1,5 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>
+<div class="mt-1 border-b border-gray-700 focus-within:border-blue-600">
+    <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'block w-full border-0 border-b border-transparent bg-gray-300 focus:border-blue-600 focus:ring-0 sm:text-sm']) !!}>
+</div>
