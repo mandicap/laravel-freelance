4261cf5cedde4ba3183a11e82abd1442b2ce6790
Amanda Romero <mandicap@comcast.net>
2021-09-14 08:19:01 -0600

    Removes logo from login page, for now

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            {{-- <x-jet-authentication-card-logo /> --}}
         </x-slot>
 
         <x-jet-validation-errors class="mb-4 p-4 bg-red-600 text-white" />
